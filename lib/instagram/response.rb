84e9e0860acec3f91f6f625db8fe04a5da41ebce
Jared Shay <jared.shay@gmail.com>
2016-02-26 13:42:46 -0800

    workaround to get ruby working on verions > 1.8

--- lib/instagram/response.rb
+++ lib/instagram/response.rb
@@ -1,8 +1,15 @@
 module Instagram
   module Response
     def self.create( response_hash, ratelimit_hash )
+      # This ensures the response_hash is never nil.
+      #
+      # Nil is frozen in ruby 1.9+ so this is required to make this gem
+      # work with any newer version of ruby.
+      response_hash = {} unless response_hash
+
       data = response_hash.data.dup rescue response_hash
       data.extend( self )
+
       data.instance_exec do
         %w{pagination meta}.each do |k|
           response_hash.public_send(k).tap do |v|
@@ -11,6 +18,7 @@ module Instagram
         end
         @ratelimit = ::Hashie::Mash.new(ratelimit_hash)
       end
+
       data
     end
 
